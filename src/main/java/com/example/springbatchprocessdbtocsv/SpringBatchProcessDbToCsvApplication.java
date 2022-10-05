220c29d72a4d6895d0ae53e811976c55b19c1950
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-10-05 22:05:20 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/example/springbatchprocessdbtocsv/SpringBatchProcessDbToCsvApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchprocessdbtocsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchProcessDbToCsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchProcessDbToCsvApplication.class, args);
+    }
+
+}
